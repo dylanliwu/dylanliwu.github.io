6fff4d76ffdebaa3c62120095d70712be20bc0d7
Dylan Li <dylanli0419@gmail.com>
2025-02-19 20:33:48 -0500

    Updated styling, improved sub-title

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
+const subLandingTitle1 = document.getElementById("subLandingTitle1");
+const subLandingTitle2 = document.getElementById("subLandingTitle2");
+
+subLandingTitle2.style.display = "none";
+
+let intervalId;
+let timeoutId;
+let isAnimating = false; // Flag to prevent multiple executions
+
+function toggleFade() {
+  if (isAnimating) return; // Prevent execution if already animating
+  isAnimating = true;
+
+  if (subLandingTitle2.classList.contains("fade-in")) {
+    subLandingTitle2.classList.remove("fade-in");
+    subLandingTitle2.classList.add("fade-out");
+    subLandingTitle1.classList.remove("fade-out");
+    subLandingTitle1.classList.add("fade-in");
+    setTimeout(() => {
+      subLandingTitle2.style.display = "none";
+      subLandingTitle1.style.display = "inline";
+      isAnimating = false; // Reset flag after animation
+    }, 300);
+  } else {
+    subLandingTitle2.classList.remove("fade-out");
+    subLandingTitle2.classList.add("fade-in");
+    subLandingTitle1.classList.remove("fade-in");
+    subLandingTitle1.classList.add("fade-out");
+    setTimeout(() => {
+      subLandingTitle2.style.display = "inline";
+      subLandingTitle1.style.display = "none";
+      isAnimating = false; // Reset flag after animation
+    }, 300);
+  }
+}
+
+function startFadeInterval() {
+  timeoutId = setTimeout(() => {
+    intervalId = setInterval(toggleFade, 3500);
+    subLandingTitle1.classList.remove("slide-down");
+  }, 1250);
+}
+
+function stopFadeInterval() {
+  clearInterval(intervalId);
+  clearTimeout(timeoutId);
+}
+
+startFadeInterval();
+
+subLandingTitle1.addEventListener("mouseenter", stopFadeInterval);
+subLandingTitle2.addEventListener("mouseenter", stopFadeInterval);
+
+subLandingTitle1.addEventListener("mouseleave", startFadeInterval);
+subLandingTitle2.addEventListener("mouseleave", startFadeInterval);
+
+subLandingTitle1.addEventListener("click", toggleFade);
+subLandingTitle2.addEventListener("click", toggleFade);
+
 const projectContainer = document.querySelector(".projects-container");
 
 const boxes = document.querySelectorAll(".project");

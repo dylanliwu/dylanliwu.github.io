d646abbf9b17edab3fb2c774be87d225636b6b3f
Dylan Li <dylanli0419@gmail.com>
2025-03-29 23:12:46 -0400

    feat: added new projects

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const navButton2 = document.getElementById("navButton2");
 const navButton3 = document.getElementById("navButton3");
 const navButton4 = document.getElementById("navButton4");
 const navButton5 = document.getElementById("navButton5");
+const navButton6 = document.getElementById("navButton6");
+const navButton7 = document.getElementById("navButton7");
 
 const primaryProjectTitle = document.getElementById("primaryProjectTitle");
 
@@ -27,8 +29,10 @@ projectContainer.addEventListener("scroll", () => {
       navButton3.style.width = '10px';
       navButton4.style.width = '10px';
       navButton5.style.width = '10px';
-      primaryProjectTitle.classList.remove("fade-out")
-      primaryProjectTitle.classList.add("fade-in")
+      navButton6.style.width = '10px';
+      navButton7.style.width = '10px';
+      primaryProjectTitle.classList.remove("fade-out");
+      primaryProjectTitle.classList.add("fade-in");
     }
     if (box.id.indexOf("project2") !== -1 && isVisible(box)) {
       index = 2;
@@ -37,8 +41,10 @@ projectContainer.addEventListener("scroll", () => {
       navButton3.style.width = '10px';
       navButton4.style.width = '10px';
       navButton5.style.width = '10px';
-      primaryProjectTitle.classList.remove("fade-in")
-      primaryProjectTitle.classList.add("fade-out")
+      navButton6.style.width = '10px';
+      navButton7.style.width = '10px';
+      primaryProjectTitle.classList.remove("fade-in");
+      primaryProjectTitle.classList.add("fade-out");
     }
     if (box.id.indexOf("project3") !== -1 && isVisible(box)) {
       index = 3;
@@ -47,6 +53,8 @@ projectContainer.addEventListener("scroll", () => {
       navButton3.style.width = '30px';
       navButton4.style.width = '10px';
       navButton5.style.width = '10px';
+      navButton6.style.width = '10px';
+      navButton7.style.width = '10px';
     }
     if (box.id.indexOf("project4") !== -1 && isVisible(box)) {
       index = 4;
@@ -55,6 +63,8 @@ projectContainer.addEventListener("scroll", () => {
       navButton3.style.width = '10px';
       navButton4.style.width = '30px';
       navButton5.style.width = '10px';
+      navButton6.style.width = '10px';
+      navButton7.style.width = '10px';
     }
     if (box.id.indexOf("project5") !== -1 && isVisible(box)) {
       index = 5;
@@ -63,6 +73,28 @@ projectContainer.addEventListener("scroll", () => {
       navButton3.style.width = '10px';
       navButton4.style.width = '10px';
       navButton5.style.width = '30px';
+      navButton6.style.width = '10px';
+      navButton7.style.width = '10px';
+    }
+    if (box.id.indexOf("project6") !== -1 && isVisible(box)) {
+      index = 6;
+      navButton1.style.width = '10px';
+      navButton2.style.width = '10px';
+      navButton3.style.width = '10px';
+      navButton4.style.width = '10px';
+      navButton5.style.width = '10px';
+      navButton6.style.width = '30px';
+      navButton7.style.width = '10px';
+    }
+    if (box.id.indexOf("project7") !== -1 && isVisible(box)) {
+      index = 7;
+      navButton1.style.width = '10px';
+      navButton2.style.width = '10px';
+      navButton3.style.width = '10px';
+      navButton4.style.width = '10px';
+      navButton5.style.width = '10px';
+      navButton6.style.width = '10px';
+      navButton7.style.width = '30px';
     }
   });
 });
@@ -81,6 +113,8 @@ navButton2.addEventListener("click", () => scrollToProject(2));
 navButton3.addEventListener("click", () => scrollToProject(3));
 navButton4.addEventListener("click", () => scrollToProject(4));
 navButton5.addEventListener("click", () => scrollToProject(5));
+navButton6.addEventListener("click", () => scrollToProject(6));
+navButton7.addEventListener("click", () => scrollToProject(7));
 
 document.addEventListener("DOMContentLoaded", () => {
   const navBar = document.querySelector(".nav-bar");
@@ -91,4 +125,4 @@ document.addEventListener("DOMContentLoaded", () => {
 const date = new Date();
 let year = date.getFullYear();
 let footerText = document.getElementById("footerText");
-footerText.innerHTML = year + " @ Dylan Li"
+footerText.innerHTML = year + " @ Dylan Li";
